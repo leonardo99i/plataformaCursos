6c4043f888e48eba7ffaaa3047740e2f0f7cc96f
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-04-12 12:53:25 -0300

    Criando um esboço da pagina de login

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Olá, mundo!'
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
