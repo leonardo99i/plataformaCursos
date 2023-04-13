400ba1545551c7e195d45778fd3390ba2d3ba533
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-04-13 17:16:25 -0300

    Ajustando o map route das páginas

--- app.py
+++ app.py
@@ -3,7 +3,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
