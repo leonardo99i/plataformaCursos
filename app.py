ab776d6831e0f6b77ad261639ac4a0a46c8c0c1b
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2023-04-15 13:12:16 -0300

    Trabalhando no back-end

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Verificar credenciais de login
+        # Se as credenciais estiverem corretas, redirecionar para a página home
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+@app.route('/home')
 def home():
     return render_template('home.html')
 
@@ -29,14 +43,15 @@ def cadastro():
 def cursos():
     return render_template('cursoss.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/pagamento')
+@app.route('/pagamento', methods=['GET', 'POST'])
 def pagamento():
-    return render_template('pagamento.html')
-
+    if request.method == 'POST':
+        # Verificar pagamento
+        # Se o pagamento estiver confirmado, redirecionar para a página de login
+        return redirect(url_for('login'))
+    else:
+        return render_template('pagamento.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
